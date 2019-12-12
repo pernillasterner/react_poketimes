71404fcad14199a79bc22147ea68b14413071356
Pernilla Sterner <pernillasterner@MBPsomtPernilla.wlan.int.chas.se>
2019-12-12 12:16:50 +0100

    Importing img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 
 class Home extends Component {
@@ -10,7 +11,7 @@ class Home extends Component {
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(res => {
-                console.log(res);
+                // console.log(res);
                 this.setState({
                     posts: res.data.slice(0,10)
                 })
@@ -22,9 +23,10 @@ class Home extends Component {
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
+                        <img src={ Pokeball } alt="A pokeball"/>
                         <div className="card-content">
                             <Link to={'/' + post.id}>
-                            <span className="card-title">{post.title}</span>
+                            <span className="card-title red-text">{post.title}</span>
                             </Link>
                             <p>{post.body}</p>
                         </div>
--- src/components/Post.js
+++ src/components/Post.js
@@ -11,16 +11,23 @@ class Post extends Component {
         axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
             .then(res => {
                 this.setState({
-                    id: id
+                    post: res.data
                 })
-                console.log(res)
+                // console.log(res)
             })
     }
     render() {
-
+        const post = this.state.post ? (
+            <div className="post">
+                <h4 className="center">{ this.state.post.title }</h4>
+                <p>{ this.state.post.body }</p>
+            </div>
+        ) : (
+            <div className="center">Loading post....</div>
+        )
         return (
             <div className="container">
-                <h4>{this.state.id}</h4>
+                <h4>{ post }</h4>
             </div>
         )
     }
